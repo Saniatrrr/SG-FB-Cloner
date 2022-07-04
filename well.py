5277d1002ac1a6ac126aa0d0124b71b7503082a6
Saniatrrr <103622470+Saniatrrr@users.noreply.github.com>
2022-07-04 19:58:52 +0600

    Add files via upload

---
+++ well.py
@@ -0,0 +1,16 @@
+import os
+os.system('clear')
+USER = "MAHABUB"
+print("\u001b[32;1mUSER_NAME IS :::---")
+print(USER)
+print("\u001b[32;1m__________𝕨𝕖𝕝𝕝𝕔𝕠𝕞𝕖__________/n")
+
+Name = input("USER_NAME:")
+
+if Name==USER :
+    os.system("python mahabub.py")
+    os.system('clear')
+else:
+    print("wrong USER__NAME")
+    os.system("python run.py")
+
